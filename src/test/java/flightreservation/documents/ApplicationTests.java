98a6687f91985cc64f500ce8ba6985cd9743029c
Faheem Shah <shahfaheem880@gmail.com>
2021-11-16 11:36:18 +0500

    Made new Project to save documents files

---
+++ src/test/java/flightreservation/documents/ApplicationTests.java
@@ -0,0 +1,13 @@
+package flightreservation.documents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
