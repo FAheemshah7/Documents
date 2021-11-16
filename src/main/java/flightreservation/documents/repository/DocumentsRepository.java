3ef01661c722b6337a173ffa2c46731f43bd3cd6
Faheem Shah <shahfaheem880@gmail.com>
2021-11-16 11:46:25 +0500

    Made Repository for Documents

---
+++ src/main/java/flightreservation/documents/repository/DocumentsRepository.java
@@ -0,0 +1,7 @@
+package flightreservation.documents.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DocumentsRepository extends CrudRepository<T, ID> {
+
+}
